68201daef11ceceb26af4de8d86685a6172ebafc
imath <math.viet@gmail.com>
2017-09-14 16:29:01 +0200

    Improve how fields containing options are rendered and fix an issue.
    The issue was the group profile informations were also saved in the xprofile data table

--- inc/functions.php
+++ inc/functions.php
@@ -100,6 +100,17 @@ function profil_de_groupes_register_group_extension() {
 add_action( 'bp_init', 'profil_de_groupes_register_group_extension' );
 
 /**
+ * Temporary filter to shortcircuit BP_XProfile_ProfileData->save().
+ *
+ * @since 1.0.0
+ *
+ * @param  BP_XProfile_ProfileData $field_data The field data object.
+ */
+function profil_de_groupes_save_field_data( $field_data ) {
+	$field_data->user_id = $field_data->field_id = 0;
+}
+
+/**
  * Sets field data for a specific field and a specific group.
  *
  * @since 1.0.0
@@ -111,15 +122,11 @@ add_action( 'bp_init', 'profil_de_groupes_register_group_extension' );
  * @return boolean                 True on success, false on failure.
  */
 function profil_de_groupes_set_field_data( $field_id = 0, $group_id = 0, $value = '', $is_required = false ) {
-	add_action( 'xprofile_data_before_save', '__return_true' );
+	add_action( 'xprofile_data_before_save', 'profil_de_groupes_save_field_data' );
 
 	$validated_field = xprofile_set_field_data( $field_id, $group_id, $value, $is_required );
 
-	remove_action( 'xprofile_data_before_save', '__return_true' );
-
-	if ( ! $validated_field ) {
-		return false;
-	}
+	remove_action( 'xprofile_data_before_save', 'profil_de_groupes_save_field_data' );
 
 	$field_data = new Profil_De_Groupes_Group_Data;
 	$field_data->field_id = $field_id;
@@ -167,34 +174,118 @@ function profil_de_groupes_fetch_fields_data() {
 }
 
 /**
- * Set the selected option in a dropdown field type.
+ * Set the selected option/checked value in field containing options.
  *
  * @since 1.0.0
  *
- * @param  string  $output   Option tag for the value being rendered.
+ * @param  string  $output   Tag for the value being rendered.
  * @param  object  $option   The option object being rendered for.
  * @param  integer $field_id ID of the field object being rendered.
- * @return string            Option tag for the value being rendered.
+ * @return string            Tag for the value being rendered.
  */
-function profil_de_groupes_set_options_select( $output = '', $option = null, $field_id = 0 ) {
+function profil_de_groupes_set_options_field( $output = '', $option = null, $field_id = 0, $selected = '', $i = 0 ) {
 	global $group;
 
 	if ( empty( $group->fields ) || empty( $option->name ) ) {
 		return $output;
 	}
 
-	$current_option = wp_filter_object_list( $group->fields, array( 'id' => $field_id ), 'and', 'data' );
-	$current_option = reset( $current_option );
+	$current_field = wp_filter_object_list( $group->fields, array( 'id' => $field_id ), 'and' );
+	$current_field = reset( $current_field );
 
-	if ( $current_option->value === $option->name ) {
-		$output = sprintf(
-			'<option selected="selected" value="%1$s">%2$s</option>',
-			esc_attr( stripslashes( $option->name ) ),
-			esc_html( stripslashes( $option->name ) )
-		);
+	if ( ! isset( $current_field->data->value ) ) {
+		return $output;
+	}
+
+	$options = maybe_unserialize( $current_field->data->value );
+
+	if ( is_a( $current_field->type_obj, 'BP_XProfile_Field_Type_Checkbox' ) || is_a( $current_field->type_obj, 'BP_XProfile_Field_Type_Radiobutton' ) ) {
+		$input_name = bp_get_the_profile_field_input_name();
+
+		if ( ! empty( $current_field->type_obj->supports_multiple_defaults ) ) {
+			$input_name .= '[]';
+		}
+
+		if ( in_array( $option->name, (array) $options, true ) ) {
+			$output = sprintf( '<label for="%2$s" class="option-label"><input checked="checked" type="%5$s" name="%1$s" id="%2$s" value="%3$s">%4$s</label>',
+				esc_attr( $input_name ),
+				esc_attr( "field_{$option->id}_{$i}" ),
+				esc_attr( stripslashes( $option->name ) ),
+				esc_html( stripslashes( $option->name ) ),
+				esc_attr( $current_field->type_obj->field_obj->type )
+			);
+		}
+	} else {
+		if ( in_array( $option->name, (array) $options, true ) ) {
+			$output = sprintf(
+				'<option selected="selected" value="%1$s">%2$s</option>',
+				esc_attr( stripslashes( $option->name ) ),
+				esc_html( stripslashes( $option->name ) )
+			);
+		}
 	}
 
 	return $output;
+}
+
+/**
+ * Set the selected options for the date field.
+ *
+ * @since 1.0.0
+ *
+ * @param  string  $html     HTML output for the date field.
+ * @param  string  $type     Which date type is being rendered for.
+ * @param  string  $day      Date formatted for the current day.
+ * @param  string  $month    Date formatted for the current month.
+ * @param  string  $year     Date formatted for the current year.
+ * @param  integer $field_id ID of the field object being rendered.
+ * @param  string  $date     Current date.
+ * @return string            HTML output for the date field.
+ */
+function profil_de_groupes_set_options_date( $html = '', $type = '', $day = '', $month = '', $year = '', $field_id = 0, $date = '' ) {
+	global $group;
+
+	if ( empty( $group->fields ) ) {
+		return $html;
+	}
+
+	$current_field = wp_filter_object_list( $group->fields, array( 'id' => $field_id ), 'and' );
+	$current_field = reset( $current_field );
+
+	if ( ! isset( $current_field->data->value ) ) {
+		return $html;
+	}
+
+	$date = $current_field->data->value;
+
+	$types = array(
+		'day'   => 'j',
+		'month' => 'F',
+		'year'  => 'Y',
+	);
+
+	// Set day, month, year defaults.
+	if ( ! empty( $date ) ) {
+		// If Unix timestamp.
+		if ( is_numeric( $date ) ) {
+			$current = date( $types[ $type ], $date );
+
+		// If MySQL timestamp.
+		} else {
+			$current = mysql2date( $types[ $type ], $date, false );
+		}
+
+		$selected = ' selected="selected"';
+		$html     = str_replace( $selected, '', $html );
+		$find     = addcslashes( sprintf( '/value="%s"/', $current ), '"' );
+
+		if ( preg_match( $find, $html, $match ) ) {
+			$match = reset( $match );
+			$html  = str_replace( $match, $match.$selected, $html );
+		}
+	}
+
+	return $html;
 }
 
 /**
@@ -205,7 +296,13 @@ function profil_de_groupes_set_options_select( $output = '', $option = null, $fi
  * @todo as it only takes care of Dropdowns list, take care of all other kind of fields.
  */
 function profil_de_groupes_add_field_filters() {
-	add_filter( 'bp_get_the_profile_field_options_select', 'profil_de_groupes_set_options_select', 10, 3 );
+	add_filter( 'bp_get_the_profile_field_options_select',      'profil_de_groupes_set_options_field', 10, 3 );
+	add_filter( 'bp_get_the_profile_field_options_checkbox',    'profil_de_groupes_set_options_field', 10, 5 );
+	add_filter( 'bp_get_the_profile_field_options_radio',       'profil_de_groupes_set_options_field', 10, 5 );
+	add_filter( 'bp_get_the_profile_field_options_multiselect', 'profil_de_groupes_set_options_field', 10, 5 );
+
+	// This one is particular
+	add_filter( 'bp_get_the_profile_field_datebox', 'profil_de_groupes_set_options_date', 10, 7 );
 }
 
 /**
@@ -216,5 +313,11 @@ function profil_de_groupes_add_field_filters() {
  * @todo as it only takes care of Dropdowns list, take care of all other kind of fields.
  */
 function profil_de_groupes_remove_field_filters() {
-	remove_filter( 'bp_get_the_profile_field_options_select', 'profil_de_groupes_set_options_select', 10, 3 );
+	remove_filter( 'bp_get_the_profile_field_options_select',      'profil_de_groupes_set_options_field', 10, 3 );
+	remove_filter( 'bp_get_the_profile_field_options_checkbox',    'profil_de_groupes_set_options_field', 10, 5 );
+	remove_filter( 'bp_get_the_profile_field_options_radio',       'profil_de_groupes_set_options_field', 10, 5 );
+	remove_filter( 'bp_get_the_profile_field_options_multiselect', 'profil_de_groupes_set_options_field', 10, 5 );
+
+	// This one is particular
+	remove_filter( 'bp_get_the_profile_field_datebox', 'profil_de_groupes_set_options_date', 10, 7 );
 }
